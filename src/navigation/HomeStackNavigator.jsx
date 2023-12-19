ee70c6b4e9df8d1cf2a372b023d18a440e9d1b72
s22001 <s22001@std.it-college.ac.jp>
2023-12-19 11:54:23 +0900

    画面をボトムタブに追加した

--- src/navigation/HomeStackNavigator.jsx
+++ src/navigation/HomeStackNavigator.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import HomeScreen from "../screens/HomeScreen"
+import MovieList from "../screens/MovieList";
+import MovieDetail from "../screens/MovieDetail";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { TouchableOpacity, StyleSheet } from "react-native";
+import SearchMovie from "../screens/SearchMovie";
 
 const Stack = createStackNavigator();
 const RootStack = createStackNavigator();
@@ -8,20 +12,73 @@ const RootStack = createStackNavigator();
 const MainStack = () => {
   return (
     <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-        />
-        {/* <Stack.Screen name="Shop" component={ShopScreen} />  */}
-      </Stack.Navigator>
+      <Stack.Screen
+        name="MovieList"
+        component={MovieList}
+        options={({ navigation }) => ({
+          title: "映画一覧",
+          headerStyle: {
+            backgroundColor: "#202328",
+          },
+          headerTintColor: "#fff",
+          headerTitleAlign: "center",
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={() => navigation.navigate("SearchMovie")}
+            >
+              <Ionicons
+                style={style.all}
+                name="search"
+                size={14}
+                color="#ccc"
+              />
+            </TouchableOpacity>
+          ),
+        })}
+      />
+      <Stack.Screen
+        name="SearchMovie"
+        component={SearchMovie}
+        options={{
+          title: "映画検索",
+          headerStyle: {
+            backgroundColor: "#202328",
+          },
+          headerTintColor: "#fff",
+          headerTitleAlign: "center",
+        }}
+      />
+      <Stack.Screen
+        name="MovieDetail"
+        component={MovieDetail}
+        options={{
+          title: "映画詳細",
+          headerStyle: {
+            backgroundColor: "#202328",
+          },
+          headerTintColor: "#fff",
+          headerTitleAlign: "center",
+        }}
+      />
+    </Stack.Navigator>
   );
 };
 
 export const HomeStackNavigator = () => (
   <RootStack.Navigator>
-      <RootStack.Group screenOptions={{ presentation: 'modal' }}>
-        <RootStack.Screen name="Main" component={MainStack} options={{ headerShown:false}}/>
-        {/* <RootStack.Screen name="CreateReview" component={CreateReviewScreen} /> */}
-      </RootStack.Group>
-    </RootStack.Navigator>
+    <RootStack.Group screenOptions={{ presentation: "modal" }}>
+      <RootStack.Screen
+        name="Main"
+        component={MainStack}
+        options={{ headerShown: false }}
+      />
+      {/* <RootStack.Screen name="CreateReview" component={CreateReviewScreen} /> */}
+    </RootStack.Group>
+  </RootStack.Navigator>
 );
+
+const style = StyleSheet.create({
+  all: {
+    marginRight: 10,
+  },
+});
