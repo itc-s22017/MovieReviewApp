dd68fbeb4002a5b61cf6a679f0a02e6c2cc3bbdd
s22017 <s22017@std.it-college.ac.jp>
2024-01-25 10:40:36 +0900

    ScrollViewとFlatListを同時に使ったときに、同じ方向のスクロール指定があるとエラーになってたのを解消した

--- src/screens/UserScreen.js
+++ src/screens/UserScreen.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { View, Text, Pressable, ScrollView,StyleSheet } from 'react-native'
+import { View, Text, Pressable, ScrollView, StyleSheet, Image,FlatList } from 'react-native'
 import { UserContext } from '../context/UserContext'
 import { signOut } from 'firebase/auth';
 import { auth } from '../../firebase';
@@ -12,6 +12,13 @@ const db = getFirestore()
 export const UserScreen = ({ navigation }) => {
   const { setUser, user } = useContext(UserContext);
   const [testid, setTestId] = useState([])
+  const userAvatarSource = user.photoURL
+    ? { uri: user.photoURL }
+    : { uri: 'https://sp-ao.shortpixel.ai/client/q_lossless,ret_img,w_250/https://miamistonesource.com/wp-content/uploads/2018/05/no-avatar-25359d55aa3c93ab3466622fd2ce712d1.jpg' };
+
+  const likesMovie = testid.map(v => (
+    <MovieFlatList url={generateSearchByIdUrl(v)} listName={''} navigation={navigation} key={v}></MovieFlatList>
+  ))
 
   useEffect(() => {
     const test = async () => {
@@ -29,10 +36,6 @@ export const UserScreen = ({ navigation }) => {
     test()
   }, [user])
 
-  const likesMovie = testid.map(v => (
-    <MovieFlatList url={generateSearchByIdUrl(v)} listName={''} navigation={navigation} key={v}></MovieFlatList>
-  ))
-
   const handleLogout = () => {
     signOut(auth)
       .then(() => {
@@ -45,6 +48,7 @@ export const UserScreen = ({ navigation }) => {
   };
   return (
     <View style={styles.container}>
+      <Image source={userAvatarSource} style={styles.userAvatar} />
       <Text>{user.displayName}</Text>
       <Pressable
         onPress={handleLogout}
@@ -59,9 +63,11 @@ export const UserScreen = ({ navigation }) => {
         <Text style={{ color: 'white' }}>ログアウト</Text>
       </Pressable>
       <Text style={styles.likesTitle}>いいねした作品</Text>
-      <ScrollView horizontal={true} contentContainerStyle={styles.scrollView}>
-        {likesMovie}
-      </ScrollView>
+      <FlatList
+        data={testid}
+        horizontal
+        renderItem={({ item }) => <MovieFlatList url={generateSearchByIdUrl(item)} listName={''} navigation={navigation} key={item}></MovieFlatList>}
+      />
     </View>
   );
 };
@@ -70,12 +76,20 @@ const styles = StyleSheet.create({
   container: {
     padding: 16,
   },
+  userInfo: {
+    flexDirection: 'row', // 横に並べる
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  userAvatar: {
+    width: 24,
+    height: 24,
+    borderRadius: 12,
+    marginRight: 8,
+  },
   likesTitle: {
     fontSize: 20,
     fontWeight: 'bold',
-    marginTop:15
-  },
-  scrollView: {
-    marginTop:-20
+    marginTop: 15,
   },
 });
