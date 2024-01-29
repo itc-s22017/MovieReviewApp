60084d5b401a3a9f7e478f97002537173d9ff117
s22017 <s22017@std.it-college.ac.jp>
2024-01-29 11:27:00 +0900

    アラートOKだけだったが、OK/Cancelの選択肢も作った

--- src/utils/showAlert.js
+++ src/utils/showAlert.js
@@ -1,7 +1,24 @@
 import { Alert } from 'react-native';
 
-export const showAlert = (title, message, buttonsText = 'OK') => {
+export const showAlert = (title, message, buttonsText = 'OK', onPress = () => { }) => {
     Alert.alert(title, message, [
-        { text: buttonsText },
+        {
+            text: buttonsText,
+            onPress: () => onPress(),
+        },
+    ]);
+};
+
+export const cancelAlert = (title, message, onPress = () => { }) => {
+    Alert.alert(title, message, [
+        {
+            text: 'Cancel',
+            onPress: () => {},
+            style: 'cancel',
+        },
+        {
+            text: "OK",
+            onPress: () => onPress(),
+        },
     ]);
 };
