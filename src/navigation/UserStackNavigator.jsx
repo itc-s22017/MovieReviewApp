937456da9fcab5357b84d11b810196166720f2eb
s22017 <s22017@std.it-college.ac.jp>
2024-01-30 11:39:25 +0900

    バグ修正

--- src/navigation/UserStackNavigator.jsx
+++ src/navigation/UserStackNavigator.jsx
@@ -13,7 +13,7 @@ export const UserStack = () => {
     return (
         <Stack.Navigator>
             <Stack.Screen
-                name="User"
+                name="UserScreen"
                 component={UserScreen}
                 options={{
                     headerStyle: {
@@ -37,7 +37,7 @@ export const UserStack = () => {
                     // headerShown:false
                 }}
             />
-            {/* <Stack.Screen
+            <Stack.Screen
                 name="UScreen"
                 component={UserScreen}
                 options={{
@@ -48,7 +48,7 @@ export const UserStack = () => {
                     headerTintColor: "#fff",
                     headerTitleAlign: "center",
                 }}
-            /> */}
+            />
             <Stack.Screen
                 name="CreateReviewScreen"
                 component={CreateReviewScreen}
